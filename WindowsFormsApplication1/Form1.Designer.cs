4d6ada2fc127c93d98f8878ce80171a8e68c98c6
BT425 <btorbert9621@gmail.com>
2014-09-09 17:04:46 -0700

    Reverse a String
    Reverse A String in C#

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(39, 37);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(237, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(39, 75);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(120, 75);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(201, 75);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Location = new System.Drawing.Point(36, 118);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(0, 13);
+            this.displayLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 173);
+            this.Controls.Add(this.displayLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Reverse a String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label displayLabel;
+    }
+}
+
---
+++ WindowsFormsApplication1/ReverseStringForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = reverseString(inputTextBox.Text);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = "";
+            inputTextBox.Text = "";
+            inputTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        ////******************* Version 1 ************************
+        //public string reverseString(string input)
+        //{
+        //    if (string.IsNullOrEmpty(input))
+        //    {
+        //        return input;
+        //    }
+
+        //    StringBuilder input_sb = new StringBuilder(input.Length);
+        //    for (int i = input.Length - 1; i >= 0; i--)
+        //    {
+        //        input_sb.Append(input[i]);
+        //    }
+
+        //    return input_sb.ToString();
+        //}
+
+        ////******************* Version 2 ************************
+        //public string reverseString(string input)
+        //{
+        //    if (input == null) return null;
+
+        //    char[] array = input.ToCharArray();
+        //    Array.Reverse(array);
+        //    return new String(array);
+        //}
+
+
+        ////******************* Version 3 ************************
+        //public string reverseString(string input)
+        //{
+        //    if (input == null) return null;
+
+        //    return new string(input.Reverse().ToArray());
+        //}
+
+        //******************* Version 4 ************************       
+        public string reverseString(string input)
+        {
+            if (input == null) return null;
+            Stack<char> stck = new Stack<char>(input.ToCharArray());
+            string output = "";
+            char element;
+            while (stck.Count > 0)
+            {
+                element = stck.Pop();
+                output += element;
+            }
+            return output;
+        }
+
+    }
+}
